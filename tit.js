e45c71eee0806f32262deddcab90d0122c16c76e
om meena <meenaom822@gmail.com>
2025-03-29 15:25:55 +0530

    add these files

--- tit.js
+++ tit.js
@@ -1,12 +1,13 @@
-let title=document.querySelector(".type");
-let fame="i am hero";
-let index=1;
-let typewriter=()=>{
-    let new_title=fame.slice(0,index);
-    title.innerText=new_title;
-    index++;
-    setTimeout(()=>{
-       typewriter();
-    },600)
-}
-typewriter();
+let btn=document.querySelector("#fun");
+btn.addEventListener("click",function(){
+    let universe=document.querySelector(".us");
+    let para=document.querySelector(".new");
+    para.innerText="Creating a better world requires teamwork, partnerships and collaboration as we need an entire army of companies to work together to build a better world within the next few decades.";
+   universe.appendChild(para);
+    
+    
+})
+btn.addEventListener("dblclick",function(){
+    let para=document.querySelector(".new");
+    para.innerText="";
+})
